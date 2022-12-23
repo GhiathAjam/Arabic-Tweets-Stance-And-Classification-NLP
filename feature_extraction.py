77f97bb9b708ae0e64747c4dec51d95cf9c18a5a
GhiathAjam <ghiath.ajam01@eng-st.cu.edu.eg>
2022-12-23 10:17:47 +0200

    TF-IDF & BOW now take tokenized documents

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,15 +6,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 def TFIDF(documents):
     '''
     inputs:
-        documents: this is a list of documents where each document is a string of words
+        documents: this is a list of documents where each document is a list of preprcessed tokens
     outputs:
         features: This is a 2D array  with N rows and V cols
                   where N is the number of documnets &  V is the vocab size  
                   this is a list of features of each document, so features[0] is the features of doc1
                   which is the tfidf of this document  for each word in the vocabulary.
-                  
     '''
-    vectorizer =TfidfVectorizer()
+    vectorizer =TfidfVectorizer(analyzer=lambda x: x)
     vectors= vectorizer.fit_transform(documents)
     features=vectors.todense()
     return features
@@ -23,7 +22,7 @@ def TFIDF(documents):
 def BOW(documents):
     '''
     inputs:
-        documents: this is a list of documents where each document is a string of words
+        documents: this is a list of documents where each document is a a list of preprcessed tokens
     outputs:
         features: This is a 2D array  with N rows and V cols
                   where N is the number of documnets &  V is the vocab size  
@@ -33,13 +32,13 @@ def BOW(documents):
     # count vectorizer function takes sentences as input 
     # convert it into matrix representation 
     # where each cell will be filled by the frequency of each vocab
-    vectorizer = CountVectorizer()
+    vectorizer = CountVectorizer(analyzer=lambda x: x)
     bow_model = vectorizer.fit_transform(documents)
     return bow_model.toarray()
 
 
 # Just a demmo test
-# doca ='انا طالبه  في هندسة'
-# docb= 'انا سعيدة جدا'
-# print(TFIDF([doca,docb]))
-# print(BOW([doca,docb]))
+doca ='انا طالبه  في هندسة'.split()
+docb= 'انا سعيدة جدا'.split()
+print(TFIDF([doca,docb]))
+print(BOW([doca,docb]))
