7653a2ec7a66a13c85a9c1ff963ab779353d843f
Rema <reeem.atalah1@gmail.com>
2022-12-25 18:06:55 +0200

    sg_lstm

--- feature_extraction.py
+++ feature_extraction.py
@@ -91,8 +91,8 @@ def SG(train_documents,test_documents):
     '''
     # create skip-gram model (SG) 
     # vector_size should be tuned according to the size of the vocabulary we have, could be 100 for the normal dataset
-    vectorizer_train = Word2Vec(train_documents, min_count = 1, vector_size = 700, window = 3, sg=1)
-    vectorizer_test = Word2Vec(test_documents, min_count = 1, vector_size = 700, window = 3, sg=1)
+    vectorizer_train = Word2Vec(train_documents, min_count = 1, vector_size = 300, window = 5, sg=1)
+    vectorizer_test = Word2Vec(test_documents, min_count = 1, vector_size = 300, window = 5, sg=1)
     # append the vectors of all sentences to a list
     vectors_sg_train = []
     # get the vector of all sentences 
@@ -110,6 +110,40 @@ def SG(train_documents,test_documents):
     return np.array(vectors_sg_train), np.array(vectors_sg_test)
 
 
+def SGLstm(train_documents,test_documents,max_len):
+    '''
+    inputs:
+        documents: this is a list of documents where each document is a a list of preprcessed tokens
+    outputs:
+        features:   This is a 2D array  with N rows and V cols
+                    where N is the number of words &  V is the vector size
+                    this is a list of features of each word in each document, so features[0] is the vector of word[0]
+    '''
+    # create skip-gram model (SG) 
+    # vector_size should be tuned according to the size of the vocabulary we have, could be 100 for the normal dataset
+    vectorizer_train = Word2Vec(train_documents, min_count = 1, vector_size = 300, window = 5, sg=1)
+    vectorizer_test = Word2Vec(test_documents, min_count = 1, vector_size = 300, window = 5, sg=1)
+    # append the vectors of each word in each sentence to a list
+    vectors_sg_train = []
+    # get the vector of all sentences 
+    for sentence in train_documents:
+        vectors_sg_train_sent = []
+        for word in sentence: 
+            if word in vectorizer_train.wv.index_to_key:
+                vectors_sg_train_sent.append(vectorizer_train.wv[word])
+        vectors_sg_train.append(vectors_sg_train_sent)
+ 
+    vectors_sg_test = []
+    for sentence in test_documents:
+        vectors_sg_test_sent = []
+        for word in sentence:
+            if word in vectorizer_test.wv.index_to_key:
+                vectors_sg_train_sent.append(vectorizer_test.wv[word])
+        vectors_sg_test.append(vectors_sg_test_sent)
+
+    return vectors_sg_train, vectors_sg_test
+    # return np.array(vectors_sg_train), np.array(vectors_sg_test)
+
 # def applyPCA(X,n_components=100):
 #     X_copy=X.copy()
 #     pca=PCA(n_components=n_components)
@@ -126,3 +160,21 @@ docc='انا هاله'.split()
 # print(CBOW( [doca,docb],[docc]))
 # print(TFIDF([doca,docb],[docc]))
 # print(BOW([doca,docb],[docc]))
+
+# SGLstm
+   # Train_X_sg = np.zeros((len(train_documents), max_len, 300))
+    # for i in range(len(train_documents)):
+    #     for j in range(len(train_documents[i])):
+    #         if train_documents[i][j] in vectorizer_train:
+    #             Train_X_sg[i][j] = vectorizer_train[train_documents[i][j]]
+    #         else:
+    #             print(train_documents[i][j])
+
+    
+    # Test_X_sg = np.zeros((len(test_documents), max_len, 300))
+    # for i in range(len(test_documents)):
+    #     for j in range(len(test_documents[i])):
+    #         if test_documents[i][j] in vectorizer_test:
+    #             Test_X_sg[i][j] = vectorizer_test[test_documents[i][j]]
+    #         else:
+    #             print(train_documents[i][j])
